78cf9bbe967ba22fc5533a6f36f616eca61448ec
leotrs <leo@leotrs.com>
2018-07-24 15:24:33 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,42 @@
+
+# SuNBEaM
+
+This package provides code to compute the (S)pectral (N)on-(B)acktracking
+(E)igenvalue Pseudo-(M)etric, or SuNBEaM for short.  The non-backtracking
+matrix is a matrix representation of a graph that has deep connections with
+the theory of the length spectrum.  The eigenvalues of the non-backtracking
+matrix can be effectively used to compute dissimilarity scores (or
+distances) between graphs.  For more information, see
+
+> Leo Torres, P. Suárez Serrato, and T. Eliassi-Rad, *Graph Distance from
+> the Topological View of Non-Backtracking Cycles*.
+
+All experiments and figures in this paper were generated with an earlier
+version of the code in this repository.
+
+![random eigenvalues](random_eigenvalues.png =150x150)
+
+
+# Installation
+
+To install, simply `git clone` this repository, import the `sunbeam` module
+and call the `nbd` function.  For `sunbeam` to work correctly you need to
+have installed NumPy, SciPy, NetworkX, and (optionally) Matplotlib.
+
+
+# Example
+
+A minimal example of how to use `sunbeam.py`:
+
+```
+import sunbeam
+import networkx as nx
+
+er = nx.erdos_renyi_graph(300, 0.05)
+ba = nx.barabasi_albert_graph(300, 3)
+sunbeam.nbdist(er, ba, 20)
+# <- 13.938343758008838
+```
+
+A more extensive example of the functionality provided in `sunbeam.py` can
+be found in the example notebook.
---
+++ sunbeam.py
@@ -0,0 +1,318 @@
+"""
+sunbeam.py
+-----------
+
+(S)pectral (N)on-(B)acktracking (E)igenvalue Pseudo-(M)etric. This module
+contains three kinds of functions related to the computation of distances
+between graphs using the eigenvalues of the non-backtracking matrix.
+
+1. Matrix Representations.  The function `fast_hashimoto` computes the
+Hashimoto or `non-backtracking` matrix of a graph, while `pseudo_hashimoto`
+computes a block matrix whose eigenvalues are also eigenvalues of the
+Hashimoto matrix.  The function `half_incidence` returns the half-incidence
+matrices of the graph.  These are useful when computing the Hashimoto
+matrix or its eigenvalues efficiently.  See discussion in [1].  Building
+the pseudo-Hashimoto matrix is always considerably faster than building the
+Hashimoto matrix.  Building the former takes as long as building the
+adjacency matrix of the graph.  Building the latter scales as the second
+moment of the degree distribution of the graph (i.e., graphs with
+heavy-tail degree distributions will take longer).
+
+2. Eigenvalue function.  The function `nbeigs` returns the non-backtracking
+eigenvalues of a graph.  The eigenvalues may be returned as complex
+numbers, as a 2D array, or as a 1D 'unrolled' feature vector.  Different
+formats are convenient for different purposes.  In computing the
+eigenvalues, we first obtain the 2-core of the graph by a process known as
+'shaving'.  See algorithm 5.1 of [1].
+
+3. Distance function.  The function `nbdist` takes two graphs and returns
+the distance between their non-backtracking eigenvalue spectra, as
+described in [1].
+
+[1] Leo Torres, P. Suárez Serrato, and T. Eliassi-Rad
+    Graph Distance from the Topological View of Non-Backtracking Cycles.
+
+Autor: leotrs (leo@leotrs.com)
+
+"""
+
+import numpy as np
+from numpy.linalg import norm
+import networkx as nx
+import scipy.sparse as sparse
+
+TOL = 1e-5
+
+
+#############################
+# 1. Matrix representations #
+#############################
+
+def pseudo_hashimoto(graph):
+    """Return the pseudo-Hashimoto matrix.
+
+    The pseudo Hashimoto matrix of a graph is the block matrix defined as
+    B' = [0  D-I]
+         [-I  A ]
+
+    Where D is the degree-diagonal matrix, I is the identity matrix and A
+    is the adjacency matrix.  See [1] for the relationship between the
+    Hashimoto matrix B and the pseudo-Hashimoto matrix B'.
+
+    [1] Leo Torres, P. Suárez Serrato, and T. Eliassi-Rad
+        Graph Distance from the Topological View of Non-Backtracking Cycles.
+
+    Params
+    ------
+
+    graph (nx.Graph): A NetworkX graph object.
+
+    Returns
+    -------
+
+    A sparse matrix in csr format.
+
+    """
+    # Note: the rows of nx.adjacency_matrix(graph) are in the same order as
+    # the list returned by graph.nodes().
+    degrees = graph.degree()
+    degrees = sparse.diags([degrees[n] for n in graph.nodes()])
+    adj = nx.adjacency_matrix(graph)
+    ident = sparse.eye(graph.order())
+    pseudo = sparse.bmat([[None, degrees - ident], [-ident, adj]])
+    return pseudo.asformat('csr')
+
+
+def half_incidence(graph, ordering='blocks', return_ordering=False):
+    """Return the 'half-incidence' matrices of the graph.
+
+    If the graph has n nodes and m *undirected* edges, then the
+    half-incidence matrices are two matrices, P and Q, with n rows and 2m
+    columns.  That is, there is one row for each node, and one column for
+    each *directed* edge.  For P, the entry at (n, e) is equal to 1 if node
+    n is the source (or tail) of edge e, and 0 otherwise.  For Q, the entry
+    at (n, e) is equal to 1 if node n is the target (or head) of edge e,
+    and 0 otherwise.
+
+    Params
+    ------
+
+    graph (nx.Graph): The graph.
+
+    ordering (str): If 'blocks' (default), the two columns corresponding to
+    the i'th edge are placed at i and i+m.  That is, choose an arbitarry
+    direction for each edge in the graph.  The first m columns correspond
+    to this orientation, while the latter m columns correspond to the
+    reversed orientation.  Columns in either block are sorted following
+    graph.edges().  If 'consecutive', the first two columns correspond to
+    the two orientations of the first edge, the third and fourth row are
+    the two orientations of the second edge, and so on.  In general, the
+    two columns for the i'th edge are placed at 2i and 2i+1.
+
+    return_ordering (bool): if True, return a function that maps an edge id
+    to the column placement.  That is, if ordering=='blocks', return the
+    function lambda x: (x, m+x), if ordering=='consecutive', return the
+    function lambda x: (2*x, 2*x + 1).  If False, return None.
+
+
+    Returns
+    -------
+
+    P (sparse matrix), Q (sparse matrix), ordering (function or None).
+
+
+    Notes
+    -----
+
+    The nodes in graph must be labeled by consecutive integers starting at
+    0.  This function always returns three values, regardless of the value
+    of return_ordering.
+
+    """
+    numnodes = graph.order()
+    numedges = graph.size()
+
+    if ordering == 'blocks':
+        src_pairs = lambda i, u, v: [(u, i), (v, numedges + i)]
+        tgt_pairs = lambda i, u, v: [(v, i), (u, numedges + i)]
+    if ordering == 'consecutive':
+        src_pairs = lambda i, u, v: [(u, 2*i), (v, 2*i + 1)]
+        tgt_pairs = lambda i, u, v: [(v, 2*i), (u, 2*i + 1)]
+
+    def make_coo(make_pairs):
+        """Make a sparse 0-1 matrix.
+
+        The returned matrix has a positive entry at each coordinate pair
+        returned by make_pairs, for all (idx, node1, node2) edge triples.
+
+        """
+        coords = list(zip(*(pair
+                            for idx, (node1, node2) in enumerate(graph.edges())
+                            for pair in make_pairs(idx, node1, node2))))
+        data = np.ones(2*graph.size())
+        return sparse.coo_matrix((data, coords),
+                                 shape=(numnodes, 2*numedges))
+
+    src = make_coo(src_pairs).asformat('csr')
+    tgt = make_coo(tgt_pairs).asformat('csr')
+
+    if return_ordering:
+        if ordering == 'blocks':
+            func = lambda x: (x, numedges + x)
+        else:
+            func = lambda x: (2*x, 2*x + 1)
+        return src, tgt, func
+    else:
+        return src, tgt, None
+
+
+def fast_hashimoto(graph, ordering='blocks', return_ordering=False):
+    """Make the Hashimoto (aka Non-Backtracking) matrix.
+
+    Params
+    ------
+
+    graph (nx.Graph): A NetworkX graph object.
+
+    ordering (str): Ordering used for edges (see `half_incidence`).
+
+    return_ordering (bool): If True, return the edge ordering used (see
+    `half_incidence`).  If False, only return the matrix.
+
+    Returns
+    -------
+
+    A sparse (csr) matrix.
+
+    """
+    sources, targets, ord_func = half_incidence(graph, ordering, return_ordering)
+    temp = np.dot(targets.T, sources).asformat('coo')
+    temp_coords = set(zip(temp.row, temp.col))
+
+    coords = [(r, c) for r, c in temp_coords if (c, r) not in temp_coords]
+    data = np.ones(len(coords))
+    shape = 2*graph.size()
+    hashimoto = sparse.coo_matrix((data, list(zip(*coords))), shape=(shape, shape))
+
+    if return_ordering:
+        return hashimoto.asformat('csr'), ord_func
+    else:
+        return hashimoto.asformat('csr')
+
+
+#############################
+# 2. Eigenvalue computation #
+#############################
+
+def nbeigs(graph, topk, fmt='complex'):
+    """Compute the largest non-backtracking eigenvalues of a graph.
+
+    Params
+    ------
+
+    graph (nx.Graph): The graph.
+
+    topk (int): The number of eigenvalues to compute.  The maximum number
+    of eigenvalues that can be computed is 2*n - 2, where n is the number
+    of nodes in graph.  All the other eigenvalues are equal to +-1.
+
+    fmt (str): The format of the return value.  If 'complex', return a list
+    of complex numbers, sorted by increasing absolute value.  If '2D',
+    return a 2D array of shape (topk, 2), where the first column contains
+    the real part of each eigenvalue, and the second column, the imaginary
+    part.  If '1D', return an array of shape (2*topk,) made by concatenaing
+    the two columns of the '2D' version into one long vector.
+
+    tol (float): Numerical tolerance.  Default 1e-5.
+
+    Returns
+    -------
+
+    A list or array with the eigenvalues, depending on the value of fmt.
+
+    """
+    # The eigenvalues are left untouched by removing the nodes of degree 1.
+    # Moreover, removing them makes the computations faster.  This
+    # 'shaving' leaves us with the 2-core of the graph.
+    core = shave(graph)
+    matrix = pseudo_hashimoto(core)
+    if topk > matrix.shape[0] - 1:
+        topk = matrix.shape[0] - 2
+        print('Computing only {} eigenvalues'.format(topk))
+
+    if topk < 1:
+        return np.array([[], []])
+
+    # The eigenvalues are sometimes returned in no particular order, which
+    # may yield different feature vectors for the same graph. For example,
+    # if a graph has a + ib and a - ib as eigenvalues, the eigenvalue
+    # solver may return [..., a + ib, a - ib, ...] in one call and [..., a
+    # - ib, a + ib, ...] in another call. To avoid this, we arbitrarily
+    # sort the eigenvalues first by absolute value, then by real part, then
+    # by imaginary part.
+    eigs = sparse.linalg.eigs(matrix, k=topk, return_eigenvectors=False, tol=TOL)
+    eigs = sorted(eigs, key=lambda x: x.imag)
+    eigs = sorted(eigs, key=lambda x: x.real)
+    eigs = np.array(sorted(eigs, key=norm))
+
+    if fmt.lower() == 'complex':
+        return eigs
+
+    eigs = np.array([(c.real, c.imag) for c in eigs])
+    if fmt.upper() == '2D':
+        return eigs
+
+    if fmt.upper() == '1D':
+        return eigs.T.flatten()
+
+
+def shave(graph):
+    """Return the 2-core of a graph.
+
+    Iteratively remove the nodes of degree 0 or 1, until all nodes have
+    degree at least 2.
+
+    """
+    core = graph.copy()
+    while True:
+        to_remove = [node for node, neighbors in core.adj.items()
+                     if len(neighbors) < 2]
+        core.remove_nodes_from(to_remove)
+        if len(to_remove) == 0:
+            break
+    return core
+
+
+
+###########################
+# 3. Distance computation #
+###########################
+
+def nbdist(graph1, graph2, topk):
+    """Compute the non-backtracking spectral distance.
+
+    Params
+    ------
+
+    graph1, graph2 (nx.Graph): The graphs to compare.
+
+    topk (int): The number of eigenvalues to compute and compare.
+
+    Returns
+    -------
+
+    A real number, the distance between graph1 and graph2.
+
+    Note
+    ----
+
+    If one graph has fewer eigenvalues than requested, the comparison uses
+    the most possible eigenvalues.
+
+    """
+    vals1 = nbeigs(graph1, topk, fmt='2d')
+    vals2 = nbeigs(graph2, topk, fmt='2d')
+    min_len = min(vals1.shape[0], vals2.shape[0])
+    vals1 = vals1[:min_len].reshape(-1, 1)
+    vals2 = vals2[:min_len].reshape(-1, 1)
+    return np.linalg.norm(vals1 - vals2)
